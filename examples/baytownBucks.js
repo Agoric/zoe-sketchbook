b802f5511be4c9584601f68ee663e566631bdbc0
Kate Sills <kate@agoric.com>
2020-01-22 16:24:43 -0800

    add eslint-jessie, update dependencies, add comments to baytownBucks

--- examples/baytownBucks.js
+++ examples/baytownBucks.js
@@ -1,10 +1,25 @@
 import { makeMint } from '@agoric/ertp/core/mint';
 
+// Let's create a community currency and make a payment for Alice.
+// This example uses ERTP but does not use Zoe, since no offers are
+// being made.
+
+// First, let's create a mint
 const baytownBucksMint = makeMint('BaytownBucks');
+
+// Now let's get the assay from the mint
+// Note: exported for testing purposes
 export const baytownBucksAssay = baytownBucksMint.getAssay();
+
+// Let's give ourselves a easy way of labeling a number as baytownBucks.
+// Note: exported for testing purposes
 export const baytownBucks = baytownBucksAssay.makeUnits;
 
+// Now let's create a purse to hold our community treasury funds.
 const purse = baytownBucksMint.mint(baytownBucks(1000), 'community treasury');
+
+// Let's make that payment for Alice.
+// Note: exported for testing purposes
 export const paymentForAlice = purse.withdraw(
   baytownBucks(10),
   `alice's community money`,
--- test/baytownBucks.test.js
+++ test/baytownBucks.test.js
@@ -2,6 +2,8 @@ import { test } from 'tape-promise/tape';
 
 import { paymentForAlice, baytownBucks } from '../examples/baytownBucks';
 
+// Let's make sure that the payment we would send to Alice has the
+// correct balance.
 test('gets the balance of the payment for Alice', t => {
   t.deepEquals(paymentForAlice.getBalance(), baytownBucks(10));
   t.end();
